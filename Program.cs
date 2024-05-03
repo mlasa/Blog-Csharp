54483ba413f98cfcb4b58e72045b17e532dced7b
Marcella Amorim <devabefarma4@outlook.com>
2024-05-03 17:38:13 -0300

    crud de usuarios com Dapper Contrib

--- Program.cs
+++ Program.cs
@@ -11,7 +11,21 @@ class Program
     private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=root@mlasa00;Trusted_Connection=False; TrustServerCertificate=True;";
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var user = new User(){
+            Id = 4,
+            Name = "Vim",
+            Bio = "Se inspira muito nos terminais",
+            Email = "v@gmail.com",
+            Image = "v.jpg",
+            PasswordHash = "HASH",
+            Slug = "v-term"
+        };
+
+        Console.WriteLine("Projeto: Blog\n----------------------\n\n");
+        //ReadUser(1);
+        //CreateUser(user);
+        //UpdateUser(user);
+        DeleteUser(user);
         ReadUsers();
     }
 
@@ -23,8 +37,40 @@ class Program
             Console.WriteLine($"Registros encontrados: {users.Count()}");
 
             foreach(var user in users){
-                Console.WriteLine($"* {user.Name}");
+                Console.WriteLine($"* {user.Id} | {user.Name} | {user.Email}");
             }
+        }
+    }
+
+    public static void ReadUser(int id){
+
+        using(var connection = new SqlConnection(CONNECTION_STRING)){
+            var user = connection.Get<User>(id);
+            Console.WriteLine($"* {user.Name}");
+        }
+    }
+
+    public static void UpdateUser(User user){
+
+        using(var connection = new SqlConnection(CONNECTION_STRING)){
+            var rows = connection.Update<User>(user);
+            if(rows) Console.WriteLine($"Usuário atualizado");
+            else Console.WriteLine("Não foi possível atualizar o registro");
+        }
+    }
+
+    public static void DeleteUser(User user){
+        Console.WriteLine($"Usuário apagado");
+        using(var connection = new SqlConnection(CONNECTION_STRING)){
+            var rows = connection.Delete<User>(user);
+            Console.WriteLine($"Usuário apagado");
+        }
+    }
+
+    public static void CreateUser(User user){
+        using(var connection = new SqlConnection(CONNECTION_STRING)){
+            var newUser = connection.Insert<User>(user);
+            Console.WriteLine("Cadastro realizado");
         }
     }
 }
